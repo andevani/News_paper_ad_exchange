e60056d7a224bcdf134f4a45afba1408b20aff8d
Ankur Devani <andevani@gmail.com>
2018-05-21 22:42:28 +0530

    working fix on 21May evening. It contains resolution of issue discussed on 19 May. It will not detect wrong red color boxes..

--- python_old/detect1.py
+++ python_old/detect1.py
@@ -91,7 +91,7 @@ img_hsv=cv2.cvtColor(resized, cv2.COLOR_BGR2HSV)
 
 # lower mask (0-10)
 lower_red = np.array([0,50,50])
-upper_red = np.array([10,255,255])
+upper_red = np.array([2,255,255])
 mask0 = cv2.inRange(img_hsv, lower_red, upper_red)
 
 # upper mask (170-180)
@@ -100,7 +100,8 @@ upper_red = np.array([180,255,255])
 mask1 = cv2.inRange(img_hsv, lower_red, upper_red)
 
 # join my masks
-mask = mask0+mask1
+#mask = mask0+mask1
+mask = mask0
 
 # set my output img to zero everywhere except my mask
 #output_img = image.copy()
